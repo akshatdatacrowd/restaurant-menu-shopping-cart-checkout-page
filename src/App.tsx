c540d5ebaec0b18af3b609f3358f65a34a210239
Akshat Agarwal <63671921+akshatdatacrowd@users.noreply.github.com>
2022-09-07 15:10:17 -0500

    Frontend Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Checkout from "./components/Checkout";
 import Header from "./components/Header";
